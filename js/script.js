feb34dbafa4df38317156d0d7096df368b6a7a19
Arseny <ars_escrime@mail.ru>
2021-11-10 10:28:05 +0300

    Home+Modal

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('[data-modal=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn('slow');
-    });
-    $('.modal__close').on('click', function() {
-        $('.overlay, #consultation').fadeOut('slow');
-    });
-});
